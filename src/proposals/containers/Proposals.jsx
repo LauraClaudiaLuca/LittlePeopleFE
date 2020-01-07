a27c7ccd2cd063feb342ca90daca946014b67a07
Tudor Maxim <maximtudor98@gmail.com>
2020-01-07 17:45:47 +0200

    proposals: WIP

--- src/proposals/containers/Proposals.jsx
+++ src/proposals/containers/Proposals.jsx
@@ -5,8 +5,10 @@ class Proposals extends React.Component {
 
     render() {
         return (
-            <h3> Proposals Page: A table with proposal for each day of the current week </h3>
-            <h5> Users can create a proposal for a day or vote an existing one</h5>
+            <React.Fragment>
+                <h3> Proposals Page: A table with proposal for each day of the current week </h3>
+                <h5> Users can create a proposal for a day or vote an existing one</h5>
+            </React.Fragment>
         )
     }
 }
